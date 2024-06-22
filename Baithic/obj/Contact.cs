3d293f9fa98e3901411c18ba024e55c49b144efc
trungle0806 <trungltrh@gmail.com>
2024-06-22 15:43:47 +0700

    Initial commit

---
+++ Baithic/obj/Contact.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BaiThi
+{
+    public class Contact
+    {
+    public string Name { get; set; }
+    public string Phone { get; set; }
+
+    public Contact(string name, string phone)
+    {
+        Name = name;
+        Phone = phone;
+    }
+    }
+}
